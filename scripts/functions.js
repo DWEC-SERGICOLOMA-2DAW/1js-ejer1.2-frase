efe21d6cf0a27bc61013dc3bcd34682fda967724
SergiColoma <sergicoloma23@gmail.com>
2023-09-22 18:59:04 +0200

    ejercicio terminado

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,35 @@
 'use strict'
 
 function letters(cadena) {
-
+	return cadena.length
 }
 
 function words(cadena) {
-
+	let cadenas = cadena.split(' ')
+	return cadenas.length
 }
 
 function upperString(cadena) {
-
+	return cadena.toLocaleUpperCase()
 }
 
 function titleString(cadena) {
-
+	let cadenas = cadena.split(' ')
+	let cadenaNueva = cadenas.map(cadena => cadena.charAt(0).toLocaleUpperCase() + cadena.slice(1))
+	return cadenaNueva.join(' ')
 }
 
 function backwardsLetters(cadena) {
-
+	return cadena.split('').reverse().join('')
 }
 
 function backwardsWords(cadena) {
-
+	return cadena.split(' ').reverse().join(' ')
 }
 
 function palindrome(cadena) {
-
+	cadena = cadena.toLowerCase().replace(/ /g, '')
+	return cadena === cadena.split('').reverse().join('')
 }
 
 module.exports = {
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
 'use strict'
 
+let cadena = prompt("Introduce el texto a analizar:")
 
+if(cadena == null || !cadena.trim()){
+    alert('No has introducido ningún texto')
+}else{
+    console.log('Frase: "' +cadena+'"')
+    console.log(letters(cadena) + ' letras y ' + words(cadena) + ' palabras')
+    console.log(upperString(cadena))
+    console.log(titleString(cadena))
+    console.log(backwardsLetters(cadena))
+    console.log(backwardsWords(cadena))
+    console.log((palindrome(cadena)?'Si':'No')+' es palíndromo')
+}
